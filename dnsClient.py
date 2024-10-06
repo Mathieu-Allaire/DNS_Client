498a70726f3ccff2ebfabaff9d0853c7434f6fe3
Jatin Patel <jatin_pat@outlook.com>
2024-10-06 11:01:56 -0400

    changed structure and completed parse header (not tested)

--- dnsClient.py
+++ dnsClient.py
@@ -25,7 +25,7 @@ def create_header():
     ARCOUNT = 0 # 16 bits
     
     header = struct.pack(">HHHHHH", ID, flags, QDCOUNT, ANCOUNT, NSCOUNT, ARCOUNT) # 12 bytes
-    return header, ID
+    return header, ID, RD
     
 def create_question(domaine_name, query_type):
     question = b''
@@ -49,28 +49,74 @@ def create_question(domaine_name, query_type):
     return question
         
     
-def create_query(domaine_name, query_type):
-    return create_header() + create_question(domaine_name, query_type)
-    
 def query_server(timeout, max_retries, port, server, query):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.settimeout(timeout)
     
+    
     retry_count = 0
     while retry_count < max_retries:
         try:
+            send_time = time.time() # time before sending the query
             sock.sendto(query, (server, port))
             response = sock.recvfrom(1024)
-            if response:
-                return response
+            recv_time = time.time() # time after receiving the response
+            if response: 
+                return response, recv_time - send_time, retry_count
         except:
             retry_count += 1
         
     print(f"ERROR    Maximum number of retries {max_retries} exceeded")
     return None
-        
-def parse_response():
-    return 1
+
+def parse_header(query_ID, query_RD, response):
+    
+    ID, flags, QDCOUNT, ANCOUNT, NSCOUNT, ARCOUNT = struct.unpack(">HHHHHH", header)
+    
+    if ID != query_id:
+        print(f"Error    Unexpected response, the response ID {ID} does not match the query ID {query_ID}")
+    
+    QR = (flags >> 15) & 0b1
+    OPCODE = (flags >> 11) & 0b1111
+    AA = (flags >> 10) & 0b1
+    TC = (flags >> 9) & 0b1
+    RD = (flags >> 8) & 0b1
+    RA = (flags >> 7) & 0b1
+    Z = (flags >> 4) & 0b111
+    RCODE = flags & 0b1111
+    
+    if QR != 1:
+        print("Error    Unexpected response, the QR bit does not correspond to a response message")
+    elif OPCODE != 0:
+        print("Error    Unexpected response, the OPCODE does not correspond to a standard query")
+    elif TC != 0:
+        print("Error    Unexpected response, message is truncated")
+    elif RD != query_rd:
+        print(f"Error    Unexpected response, the RD flag {RD} does not match the query RD flag {query_RD}")
+    elif Z != 0:
+        print("Error    Unexpected response, the Z field is not zero")
+    elif RCODE != 0:
+        if RCODE == 1:
+            print("Error    Format error: the name server was unable to interpret the query")
+        elif RCODE == 2:
+            print("Error    Server failure: the name server was unable to process the query")
+        elif RCODE == 3:
+            print("Error    Name error: the domain name does not exist")
+        elif RCODE == 4:
+            print("Error    Not implemented: the name server does not support the requested query")
+        elif RCODE == 5:
+            print("Error    Refused: the name server refused to process the query")
+            
+    return ID, QR, OPCODE, AA, TC, RD, RA, Z, RCODE, QDCOUNT, ANCOUNT, NSCOUNT, ARCOUNT
+                 
+def parse_response(query_ID, query_RD, question, response):
+    
+    ID, QR, OPCODE, AA, TC, RD, RA, Z, RCODE, QDCOUNT, ANCOUNT, NSCOUNT, ARCOUNT = parse_header(query_ID, query_RD, response)
+    
+    
+
+    
+    
     
 
 def main():
@@ -120,9 +166,20 @@ def main():
     print(f"Server: {server}")
     print(f"Request type: {query_type}")
     
-    query, query_id = create_query(name, query_type)
-    response = query_server(timeout, max_retries, port, server, query)
-    parse_response(query_id, response)
+    # create query
+    # Store ID, RD, and question for later use in parsing the response
+    header, ID, RD = create_header()
+    question = create_question(name, query_type)
+    query = header + question
+    
+    # send query to server
+    response, elapsed_time, retry_count = query_server(timeout, max_retries, port, server, query)
+    
+    print(f"Response received after {elapsed_time} seconds ({retry_count} retries)")
+    
+    
+    # parse response
+    parse_response(ID, RD, question, response)
 
               
 if __name__ == "__main__":
